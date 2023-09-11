acb9ac33be466ef5e7cfbebcfa87e5255aec7f79
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-09-11 23:37:17 +0800

    1

--- diypart1.sh
+++ diypart1.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #=================================================
 # cp -Rf ./.github/tmp/*  .
+ls -a
 cp -Rf ../lede/*.sh openwrt
+echo '#================start======================='
+ls -a
 chmod +x openwrt/diypart1.sh
 ./diypart1.sh
