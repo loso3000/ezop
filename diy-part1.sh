9fb010dc5fd7922108ae95ace52f09beade25b3b
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-09-12 14:06:54 +0800

    gx

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,10 +19,10 @@ sed -i 's/+luci-theme-bootstrap/+luci-theme-kucat/g' feeds/luci/collections/luci
 
 sed -i 's,media .. \"\/b,resource .. \"\/b,g' ./feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/sysauth.htm
 
-curl -fsSL  https://raw.githubusercontent.com/sirpdboy/other/master/patch/banner > ./package/base-files/files/etc/banner
-curl -fsSL  https://raw.githubusercontent.com/sirpdboy/other/master/patch/profile > ./package/base-files/files/etc/profile
-curl -fsSL  https://raw.githubusercontent.com/sirpdboy/other/master/patch/profiles > ./package/base-files/files/etc/profiles
-curl -fsSL  https://raw.githubusercontent.com/sirpdboy/other/master/patch/sysctl.conf > ./package/base-files/files/etc/sysctl.conf
+curl -fsSL  https://raw.githubusercontent.com/loso3000/other/master/patch/banner > ./package/base-files/files/etc/banner
+curl -fsSL  https://raw.githubusercontent.com/loso3000/other/master/patch/profile > ./package/base-files/files/etc/profile
+curl -fsSL  https://raw.githubusercontent.com/loso3000/other/master/patch/profiles > ./package/base-files/files/etc/profiles
+curl -fsSL  https://raw.githubusercontent.com/loso3000/other/master/patch/sysctl.conf > ./package/base-files/files/etc/sysctl.conf
 
 #修改默认IP地址
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
@@ -96,5 +96,47 @@ echo ${date1}' by Sirpdboy ' >> ./package/base-files/files/etc/banner
 
 echo '---------------------------------' >> ./package/base-files/files/etc/banner
 
+#删除冲突插件
+# rm -rf $(find ./feeds/luci/ -type d -regex ".*\(argon\|design\|openclash\).*")
+#修改默认主题
+# sed -i "s/luci-theme-bootstrap/luci-theme-$OWRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
+#修改默认IP地址
+# sed -i "s/192\.168\.[0-9]*\.[0-9]*/$OWRT_IP/g" ./package/base-files/files/bin/config_generate
+#修改默认主机名
+# sed -i "s/hostname='.*'/hostname='$OWRT_NAME'/g" ./package/base-files/files/bin/config_generate
+#修改默认时区
+sed -i "s/timezone='.*'/timezone='CST-8'/g" ./package/base-files/files/bin/config_generate
+sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
+mkdir -p files/etc/openclash/core
+CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-${1}.tar.gz"
+CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep amd64 | awk -F '"' '{print $4}' | grep "v3" )
+CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-${1}.tar.gz"
+GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+wget -qO- $CLASH_DEV_URL | tar xOvz > files/etc/openclash/core/clash
+wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
+wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
+wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
+wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
+chmod +x files/etc/openclash/core/clash*
+
 ./scripts/feeds update -i
 
+mkdir -p files/root
+pushd files/root
+
+## Install oh-my-zsh
+# Clone oh-my-zsh repository
+git clone https://github.com/ohmyzsh/ohmyzsh ./.oh-my-zsh
+
+# git clone https://github.com/robbyrussell/oh-my-zsh ./.oh-my-zsh
+# Install extra plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
+
+cp  -f ../../z.zshrc ./.zshrc
+cp  -f ../../profiles .
+popd
+mv -f ./z.zshrc ./files/root/.zshrc
+mv -f ./profiles ./files/etc/
