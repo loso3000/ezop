3c3540f08414a9bef159d68400f1c7bc0cfdb83f
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-09-15 19:30:26 +0800

    x

--- diy-part1.sh
+++ diy-part1.sh
@@ -153,8 +153,8 @@ bakkmoddir=./file/etc/kmod.d
 bakkmodfile=$bakkmoddir/kmod.source
 nowkmodfile=$bakkmoddir/kmod.now
 [ ! -d $bakkmoddi ] && mkdir -p bakkmoddi 2>/dev/null
-[ -f $bakkmodfile ] || cp -rf ./patch/kmod.source $bakkmodfile
-while IFS= read -r file; do find ./bin/ -name "$file" | xargs -i cp -f {} $bakkmoddir ; done < $bakkmodfile
+[ -f $bakkmodfile ] || cp -rf ../kmod.source $bakkmodfile
+while IFS= read -r file; do find ./bin/ -name "${file}*" | xargs -i cp -f {} $bakkmoddir ; done < $bakkmodfile
 sleep 2
 ls $bakkmoddir > $nowkmodfile
 exit
