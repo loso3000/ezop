e41d62fc3e38141747801a5daaf4e03b3938399a
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-09-15 19:17:45 +0800

    b

--- diypart1.sh
+++ diypart1.sh
@@ -7,4 +7,4 @@ cp -Rf ../lede/*.sh .
 echo '#================start======================='
 ls -a
 chmod +x diypart1.sh
-[ -e diypart1.sh ] && ./diypart1.sh || ./diy.sh
+[ -e diypart1.sh ] && bash ./diypart1.sh || bash ./diy.sh
